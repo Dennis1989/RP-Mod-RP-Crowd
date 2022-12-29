658dda8b77975864445e5c7b43120330a94f70b1
Moritz Seiler <moritzseiler@KlapprechnerAir.local>
2022-12-29 11:57:29 +0100

    Fixed small bug due to PyTorch 1.12

--- Training/Language Models/nimble_pytorch/losses/transformers.py
+++ Training/Language Models/nimble_pytorch/losses/transformers.py
@@ -40,5 +40,5 @@ class DoubleLoss(nn.modules.loss._Loss):
     def forward(self, output, batch, **kwargs):
         cls_loss = self.loss(output[self.cls_out_key], batch[self.cls_inp_key].squeeze(-1))
         mlm_loss = self.loss(output[self.mlm_out_key].transpose(1,-1), 
-                                   batch[self.mlm_inp_key], ignore_index=-100)
+                                   batch[self.mlm_inp_key], ignore_index=-100).nan_to_num() # Due to changes in PyTorch 1.12
         return self.alpha * mlm_loss + (1-self.alpha) * cls_loss
